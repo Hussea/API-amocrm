18a241d6e971929f5a457877f9b30461abaf7c3a
Hussea <alameryhh@gmail.com>
2024-04-15 18:12:33 +0300

    API-AMOCRM

---
+++ get.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$clientId = "5581328e-d3b4-4d82-84c0-46d4532753a8";
+$clientSecret = "zKzy2KdyAHEgKlB7LUoctY59Ib3vqTKHqbawfYC2IycJDXY3vUszLy5Tyl298l1O";
+$redirectUri = "https://yan.ru";
+$account_domain = "{{alnaserhussein}}.amocrm.ru";
+$apiClient = new AmoCRMApiClient($clientId, $clientSecret, $redirectUri);
+$apiClient->setAccountBaseDomain($account_domain);
+$rawToken = json_decode(file_get_contents('response.json'), 1); // данные, которые вы получили POST /oauth2/access_token
+$token = new AccessToken($rawToken);
+$apiClient->setAccessToken($token);
+
+
+
+$leadsService = $apiClient->leads();
+$lead = new LeadModel();
+$lead->setName('Название сделки')->setPrice(54321);
+$leadsCollection = new LeadsCollection();
+$leadsCollection->add($lead);
+$leadsCollection = $leadsService->add($leadsCollection);
+?>
